01703eb1c239c9a4cf32a091337558ca225582b9
grillz <grillz@grillzs-MacBook-Pro.local>
2019-08-29 11:19:19 +0200

    added dom element creation

--- app.js
+++ app.js
@@ -31,13 +31,49 @@ const user = { name: "hello", age: 18, shoes: "nikes" };
 
 displayUserInfo(user);
 
-function displayUserInfo({ name, age, shoes }) {
-  console.log("User name is ", name);
-  console.log("User age is ", age);
-  console.log("User is wearing  ", shoes);
+// function displayUserInfo({ name, age, shoes }) {
+//   console.log("User name is ", name);
+//   console.log("User age is ", age);
+//   console.log("User is wearing  ", shoes);
+// }
+
+// function displayUserInfo(user) {
+//   console.log("User name is ", user.name);
+//   console.log("User age is ", user.age);
+//   console.log("User is wearing  ", user.shoes);
+// }
+
+const showCase = document.getElementById("users-showcase");
+
+const users = [
+  { name: "hello", age: 18, image: "nikes" },
+  { name: "Mike", age: 18, image: "nikes" },
+  { name: "Toto", age: 9, image: "nikes" }
+];
+
+function createUserCard(user) {
+  const card = document.createElement("div");
+  card.classList.add("user-card");
+  //    card.className = "user-card";
+  card.innerHTML = `<div class="user-image">
+    <img src="${user.image}" </div>
+    <div class="user-info">
+        Name: ${user.name}    Age:${user.age}
+    </div>`;
+
+  return card;
 }
-function displayUserInfo(user) {
-  console.log("User name is ", user.name);
-  console.log("User age is ", user.age);
-  console.log("User is wearing  ", user.shoes);
+
+axios.get("myapiurlthatgivesmeallmyusers").then(res => {
+  const users = res.data;
+  const showCase = document.getElementById("users-showcase");
+  createUsersCards(showCase, users);
+});
+
+function createUsersCards(DOMParent, users) {
+  users.forEach(user => {
+    // showCase.apppendChild(createUserCard(user));
+    const userDOMElement = createUserCard(user);
+    DOMParent.appendChild(userDOMElement);
+  });
 }
